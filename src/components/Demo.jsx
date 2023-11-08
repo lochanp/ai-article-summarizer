637b2911af9e54d4051d47991f03442b62039afe
lochan07 <lochan@binaryveda.com>
2023-11-08 23:51:08 +0530

    added projects descriptions

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -69,6 +69,7 @@ const Demo = () => {
             ) : 
             article.summary &&
                 <div className='flex flex-col gap-3'>
+                   
                     <h2 className='font-satoshi font-bold text-gray-700 text-xl'>
                         Article <span className='blue_gradient'>Summary</span>
                     </h2>
